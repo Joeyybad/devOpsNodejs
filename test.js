d9520a39a87e0cd3507dc76086ab0c5c45de0c25
NKUNGA Jordan <jnkunga@hotmail.com>
2025-04-30 19:31:04 +0200

    ajustement test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
- const assert = require('assert');
+const assert = require('assert');
 const add = require('./index');
 
 assert.strictEqual(add(2, 3), 5);
 assert.strictEqual(add(-1, 1), 0);
 
-console.log(✅ Tests passed!);
+console.log("✅ Tests passed!");
